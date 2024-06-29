36ea8b546e9aabd65873643ef6aaa1add5aa7ede
Shayaan Jagtap <shayaan.jagtap@gmail.com>
2024-06-28 22:48:59 -0500

    debugging lines in env file for deployment debugging

--- src/information_retrieval/utils/files.py
+++ src/information_retrieval/utils/files.py
@@ -10,11 +10,12 @@ logger = get_logger()
 def load_env() -> Dict[str, str]:
     logger.debug("Loading env variables...")
     env = dict(os.environ)
+    logger.debug(f"Env variables loaded: {env}")
     return env
 
 
 def load_config_from_env(env: str) -> Dict[str, Any]:
-    logger.debug(f"Loading config from env {env}...")
+    logger.debug(f"Loading config from env: {env}...")
     env = env.replace('"', '')
     with open(f"resources/config/{env}/conf.yaml", "r") as config_file:
         config = yaml.safe_load(config_file)
