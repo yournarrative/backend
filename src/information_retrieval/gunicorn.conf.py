21dc19d9b54dcab9fce4c4b2de304fddbea61bc2
Shayaan Jagtap <shayaan.jagtap@gmail.com>
2024-07-17 17:07:29 -0500

    Reducing process count to 2 for prod

--- src/information_retrieval/gunicorn.conf.py
+++ src/information_retrieval/gunicorn.conf.py
@@ -5,7 +5,8 @@ bind = os.getenv("HOST") + ":" + os.getenv("PORT")
 if os.environ.get("ENVIRONMENT") == "local":
     workers = 1
 else:
-    workers = multiprocessing.cpu_count() * 2 + 1
+    workers = 2
+    # workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "uvicorn.workers.UvicornWorker"
 reload = True
 reload_engine = "auto"
